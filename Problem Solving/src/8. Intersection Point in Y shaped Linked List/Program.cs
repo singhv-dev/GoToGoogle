fc0a8ccd151a34cb105ccad2f1e654eb017d1bde
vikram.singh <vikram.singh@intimetec.com>
2021-10-03 12:07:08 +0530

    Intersection point and Reverse Linked list in chunks

---
+++ Problem Solving/src/8. Intersection Point in Y shaped Linked List/Program.cs
@@ -0,0 +1,63 @@
+﻿using _6._Custom_Linked_List;
+using System;
+
+namespace _8._Intersection_Point_in_Y_shaped_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int intersectPoint(LLNode<int> first, LLNode<int> second)
+        {
+            int firstLength = GetLinkedListLength(first);
+            int secondLength = GetLinkedListLength(second);
+            if (firstLength > secondLength)
+            {
+                first = MoveKPointerAhead(first, firstLength - secondLength);
+            }
+            else if (secondLength > firstLength)
+            {
+                second = MoveKPointerAhead(second, secondLength - firstLength);
+            }
+
+            while (first != null && second != null)
+            {
+                if (first.Value == second.Value)
+                {
+                    return first.Value;
+                }
+
+                first = first.Next;
+                second = second.Next;
+            }
+
+            return -1;
+        }
+
+        public static LLNode<int> MoveKPointerAhead(LLNode<int> head, int k)
+        {
+            while (k > 0 && head != null)
+            {
+                head = head.Next;
+                k--;
+            }
+
+            return head;
+        }
+
+        public static int GetLinkedListLength(LLNode<int> head)
+        {
+            int cnt = 0;
+            while (head != null)
+            {
+                cnt++;
+                head = head.Next;
+            }
+
+            return cnt;
+        }
+    }
+}
