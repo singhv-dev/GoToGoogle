47fe8d90f2ff5f28f4ef9e3150613cb2f8bf0330
vikram.singh <vikram.singh@intimetec.com>
2021-10-03 08:38:40 +0530

    Initial Stage

---
+++ Problem Solving/src/2. Detect Loop In Linked List/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Detect_Loop_In_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode<int> head = GenerateLinkedListWithoutCycle();
+            bool isCycleExists = CycleDetection.IsCycleExistsWithHashMap(head);
+            if (isCycleExists)
+            {
+                Console.WriteLine("Cycle Exists...");
+            }
+            else
+            {
+                Console.WriteLine("Cycle Does not exists...");
+            }
+        }
+
+        static LinkedListNode<int> GenerateLinkedListWithoutCycle()
+        {
+            throw new NotImplementedException();
+        }
+
+        static LinkedListNode<int> GenerateListWithCycle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class CycleDetection
+    {
+        public static bool IsCycleExistsWithTwoPointer(LinkedListNode<int> head)
+        {
+            if (head == null)
+                return false;
+
+            LinkedListNode<int> firstPointer = head, secondPointer = head;
+            while (firstPointer != null
+                && secondPointer != null
+                && secondPointer.Next != null)
+            {
+                firstPointer = firstPointer.Next;
+                secondPointer = secondPointer.Next.Next;
+                if (firstPointer == secondPointer)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public static bool IsCycleExistsWithHashMapWhenDistinctElements(LinkedListNode<int> head)
+        {
+            Dictionary<int, bool> nodeDictionary = new Dictionary<int, bool>();
+            while (head != null)
+            {
+                if (nodeDictionary.ContainsKey(head.Value))
+                {
+                    return true;
+                }
+
+                nodeDictionary.Add(head.Value, true);
+                head = head.Next;
+            }
+
+            return false;
+        }
+
+        public static bool IsCycleExistsWithHashMapWhenDuplicateElements(LinkedListNode<int> head)
+        {
+            Dictionary<LinkedListNode<int>, bool> nodeDictionary = new Dictionary<LinkedListNode<int>, bool>();
+            while (head != null)
+            {
+                if (nodeDictionary.ContainsKey(head))
+                {
+                    return true;
+                }
+
+                nodeDictionary.Add(head, true);
+                head = head.Next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Problem Solving/src/6. Custom Linked List/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _6._Custom_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LLNode<int> head = GenerateLinkedList();
+            DisplayLinkedList(head);
+            head = ReverseLinkedList(head);
+            DisplayLinkedList(head);
+        }
+
+        static LLNode<int> ReverseLinkedList(LLNode<int> head)
+        {
+            LLNode<int> reversedHead = null;
+            while (head != null)
+            {
+                LLNode<int> temp = head.Clone();
+                temp.Next = reversedHead;
+                reversedHead = temp;
+
+                head = head.Next;
+            }
+
+            return reversedHead;
+        }
+
+        static void DisplayLinkedList(LLNode<int> head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.Value);
+                head = head.Next;
+            }
+
+            Console.WriteLine("=============================");
+        }
+
+        static LLNode<int> GenerateLinkedList()
+        {
+            LLNode<int> head = null, last = null;
+            for (int i = 1; i <= 10; i++)
+            {
+                LLNode<int> temp = new LLNode<int>(i, null);
+                if (head == null)
+                {
+                    head = last = temp;
+                }
+                else
+                {
+                    last.Next = temp;
+                    last = last.Next;
+                }
+            }
+
+            return head;
+        }
+    }
+
+    public class Person: ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+    public class LLNode<T>
+    {
+        public T Value { get; set; }
+        public LLNode<T> Next { get; set; }
+
+        public LLNode(T value, LLNode<T> next = null)
+        {
+            this.Value = value;
+            this.Next = next;
+        }
+
+        public LLNode<T> Clone()
+        {
+            LLNode<T> newNode = new LLNode<T>(this.Value, this.Next);
+            return newNode;
+        }
+    }
+}
